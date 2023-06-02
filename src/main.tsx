01270c0613195c3674a62ddc09abcf38b4449adb
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-02 22:20:45 +0400

    add global styles and react helmet to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
+import { Helmet } from 'react-helmet'
+import GlobalStyle from './config/Global.styled.ts'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyle />
+    <Helmet>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com"  />
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600&display=swap" rel="stylesheet" />
+    </Helmet>
     <App />
   </React.StrictMode>,
 )
