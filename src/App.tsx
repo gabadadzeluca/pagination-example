31a7cf7d549b404fdf37b476e451a2ca969cede8
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-03 21:52:03 +0400

    add first logic of data fetching and displaying

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,26 @@ const ITEMS_PER_PAGE : number = 10;
 
 function App() {
   const [data, setData] = useState<PhotoObjectInterface[]>([]);
+  const [currentPage, setCurrentPage] = useState<number>(1);
   
   useEffect(()=>{
    fetchData();
   }, []);
 
   const fetchData = async () => {
-    // const res = await axios.get(`https://jsonplaceholder.typicode.com/photos/?_start=${0}&_limit=${5000}`);
     const res = await axios.get(`https://jsonplaceholder.typicode.com/photos/`);
     console.log(res.data.length);
     setData(res.data);
    }
 
+  const getBatchData = (start:number, end:number):PhotoObjectInterface[] => {
+    // Slice the data array to get the images for the current page
+    return data.slice(start, end);
+  };
+  
+  const handlePageChange = (pageNumber:number) =>{
+    setCurrentPage(pageNumber);
+  }
 
   return (
     <>
