eadf7e7dc7aeb5ede0f57b513c3a27d70e119946
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-03 21:23:49 +0400

    add first version of fetchData function

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,23 @@
+import { useState, useEffect } from 'react';
+import PhotoObjectInterface from './utils/PhotoObjectInterface';
+import axios from 'axios';
+
+const ITEMS_PER_PAGE : number = 10;
+
 function App() {
+  const [data, setData] = useState<PhotoObjectInterface[]>([]);
+  
+  useEffect(()=>{
+   fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    // const res = await axios.get(`https://jsonplaceholder.typicode.com/photos/?_start=${0}&_limit=${5000}`);
+    const res = await axios.get(`https://jsonplaceholder.typicode.com/photos/`);
+    console.log(res.data.length);
+    setData(res.data);
+   }
+
 
   return (
     <>
