19d62985a74641120507b6028401b5fae00d1593
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-03 23:26:45 +0400

    add 5 buttons for pagination navigation

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -1,12 +1,20 @@
-export default function Pagination(props:{
+const ACTIONS = [-5, -1, 0, 1, 5];
+
+export default function Pagination(props: {
   handlePageChange: (pageNumber: number) => void;
   currentPage: number;
 }) {
   const { currentPage, handlePageChange } = props;
+
   return (
     <div>
-      <button onClick={()=>handlePageChange(currentPage - 1)}>-</button>
-      <button onClick={()=>handlePageChange(currentPage + 1)}>+</button>
+      <button onClick={() => handlePageChange(currentPage - 1)}>Prev</button>
+      {ACTIONS.map((number) => (
+        <button onClick={() => handlePageChange(currentPage + number)}>
+          {currentPage + number < 0 ? "" : currentPage + number}
+        </button>
+      ))}
+      <button onClick={() => handlePageChange(currentPage + 1)}>Next</button>
     </div>
-  )
+  );
 }
