c0cc6d7d6b97e66637f113d350433328fda57bba
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-03 22:21:59 +0400

    format with prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,51 @@
-import { useState, useEffect } from 'react';
-import PhotoObjectInterface from './utils/PhotoObjectInterface';
-import axios from 'axios';
-import Pagination from './components/Pagination/Pagination';
-import Posts from './components/Posts/Posts';
+import { useState, useEffect } from "react";
+import PhotoObjectInterface from "./utils/PhotoObjectInterface";
+import axios from "axios";
+import Pagination from "./components/Pagination/Pagination";
+import Posts from "./components/Posts/Posts";
 
-const ITEMS_PER_PAGE : number = 10;
+const ITEMS_PER_PAGE: number = 10;
 
 function App() {
   const [data, setData] = useState<PhotoObjectInterface[]>([]);
   const [currentPage, setCurrentPage] = useState<number>(1);
-  
-  useEffect(()=>{
-   fetchData();
+
+  useEffect(() => {
+    fetchData();
   }, []);
 
   const fetchData = async () => {
     const res = await axios.get(`https://jsonplaceholder.typicode.com/photos/`);
     setData(res.data);
-   }
+  };
 
-  const getBatchData = (start:number, end:number):PhotoObjectInterface[] => {
-    if(end === 0){ // corner case 
+  const getBatchData = (start: number, end: number): PhotoObjectInterface[] => {
+    if (end === 0) {
+      // corner case
       return [...data.slice(start, -1), data[data.length - 1]];
     }
     // Slice the data array to get the images for the current page
     return data.slice(start, end);
   };
-  
-  const handlePageChange = (pageNumber:number) =>{
+
+  const handlePageChange = (pageNumber: number) => {
     setCurrentPage(pageNumber);
-  }
+  };
 
   return (
     <>
-      <Posts batchData={getBatchData(currentPage*ITEMS_PER_PAGE, (currentPage+1)*ITEMS_PER_PAGE)} />
-      <Pagination handlePageChange={handlePageChange} currentPage={currentPage} />
+      <Posts
+        batchData={getBatchData(
+          currentPage * ITEMS_PER_PAGE,
+          (currentPage + 1) * ITEMS_PER_PAGE
+        )}
+      />
+      <Pagination
+        handlePageChange={handlePageChange}
+        currentPage={currentPage}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
